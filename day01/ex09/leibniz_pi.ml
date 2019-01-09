06cdeb0c67c33e3a334e577eb64b85c038578737
douglas engstrand <doueng09@gmail.com>
2019-01-09 13:37:25 +0100

    made leibniz better

--- day01/ex09/leibniz_pi.ml
+++ day01/ex09/leibniz_pi.ml
@@ -1,21 +1,21 @@
-let leibniz_pi d =
-  let pi i =
+let leibniz_pi delta =
+  let my_pi i =
     4. *. (((-1.) ** i) /. ((2. *. i) +. 1.));
+  in
+  let pi =
+    4. *. (atan 1.)
   in
   let ft_abs_float n =
     if n > 0. then n else (-1.) *. n;
   in
-  let diff sum =
-    ft_abs_float (4. *. (atan 1.) -. sum);
-  in
-  if d < 0. then
+  if delta < 0. then
     -1
   else
     let rec loop i sum =
-      if d >= diff sum then
+      if delta >= ft_abs_float (pi -. sum) then
         i
       else
-        loop (i + 1) (sum +. (pi (float_of_int i)))
+        loop (i + 1) (sum +. (my_pi (float_of_int i)))
     in
     loop 0 0.
 
